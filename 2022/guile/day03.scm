ae392dc78c311444fc85f58c56c4aefa71415bfc
Caitlin <10053862+con-cat@users.noreply.github.com>
2022-12-03 18:32:30 +1100

    Day 3 part 2 in Guile

--- 2022/guile/day03.scm
+++ 2022/guile/day03.scm
@@ -6,22 +6,34 @@
 (define (parse-line l)
   (let ((idx (/ (string-length l) 2)))
     (let ((strings (list (substring l 0 idx) (substring l idx))))
-      (car (char-set->list (apply char-set-intersection (map string->char-set strings)))))))
+      (apply char-set-intersection (map string->char-set strings)))))
 
-(define input (stream-map parse-line (file->stream-lines "../input/day03.txt")))
+(define input (file->stream-lines "../input/day03.txt"))
 
-(define (get-priority c)
-  (if (char-set-contains? char-set:lower-case c)
-      (- (char->integer c) 96)
-      (- (char->integer c) 38)))
+(define (get-priority cset)
+  (let ((c (car (char-set->list cset))))
+    (if (char-set-contains? char-set:lower-case c)
+        (- (char->integer c) 96)
+        (- (char->integer c) 38))))
 
 (define (part-1)
-  (let loop ((input input) (sum 0))
+  (let loop ((input (stream-map parse-line input)) (sum 0))
     (if (stream-null? input)
         sum
         (loop (stream-cdr input) (+ sum (get-priority (stream-car input)))))))
 
+(define (part-2)
+  (let loop ((input (stream->list (stream-map string->char-set input))) (sum 0))
+    (if (null? input)
+        sum
+        (let ((cset (apply char-set-intersection (list-head input 3))))
+          (loop (list-tail input 3) (+ sum (get-priority cset)))))))
+
+
 
 (display "Part 1: ")
 (display (part-1))
+(newline)
+(display "Part 2: ")
+(display (part-2))
 (newline)
