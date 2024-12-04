0a88e663c43f77928ae5fd6e7a068942c41a996b
Caitlin White <caitlin@elephants.email>
2024-12-04 22:23:23 +1100

    Day 3 part 2 in Python

--- 2024/python/day03.py
+++ 2024/python/day03.py
@@ -1,8 +1,7 @@
 import re
-from collections.abc import Sequence
 
 
-def part_1(instructions: Sequence[str]) -> int:
+def part_1(instructions: list[str]) -> int:
     total = 0
     for line in instructions:
         for num_1, num_2 in re.findall(r"mul\((\d+),(\d+)\)", line):
@@ -11,8 +10,30 @@ def part_1(instructions: Sequence[str]) -> int:
     return total
 
 
+def part_2(instructions: list[str]) -> int:
+    total = 0
+    should_multiply = True
+    for line in instructions:
+        statements = re.findall(r"mul\(\d+,\d+\)|do\(\)|don't\(\)", line)
+        for statement in statements:
+            if statement.startswith("don't"):
+                should_multiply = False
+            elif statement.startswith("do"):
+                should_multiply = True
+            elif should_multiply:
+                num_1, num_2 = re.findall(r"\d+", statement)
+                total += int(num_1) * int(num_2)
+
+    return total
+
+
 if __name__ == "__main__":
     input_path = "../input/day03"
     with open(input_path, "r", encoding="utf-8") as f:
-        print("Part 1:")
-        print(part_1(f))
+        instructions = list(f)
+
+    print("Part 1:")
+    print(part_1(instructions))
+
+    print("Part 2:")
+    print(part_2(instructions))
