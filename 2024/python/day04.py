b4f4daadb20adbc2102f689a2d55758b55195479
Caitlin White <caitlin@elephants.email>
2024-12-15 15:58:32 +1100

    Day 4 part 2 in Python

--- 2024/python/day04.py
+++ 2024/python/day04.py
@@ -11,6 +11,8 @@ MXMXAXMASX"""
 
 XMAS = ["X", "M", "A", "S"]
 
+MAS_MAP = {"M": "S", "S": "M"}
+
 
 def count_xmas(matrix: list[list[str]]) -> int:
     count = 0
@@ -23,6 +25,7 @@ def count_xmas(matrix: list[list[str]]) -> int:
 
 
 def rotate_45(matrix: list[list[str]]) -> list[list[str]]:
+    # https://algocademy.com/blog/matrix-traversal-mastering-spiral-diagonal-and-zigzag-patterns/
     rows = len(matrix)
     cols = len(matrix[0])
     result = []
@@ -65,12 +68,41 @@ def part_1(matrix: list[list[str]]) -> int:
     return count
 
 
+def part_2(matrix: list[list[str]]) -> int:
+    count = 0
+    rows = len(matrix)
+    cols = len(matrix[0])
+    for y in range(1, rows - 1):
+        for x in range(1, cols - 1):
+            if matrix[y][x] != "A":
+                continue
+
+            top_left = matrix[y - 1][x - 1]
+            top_right = matrix[y - 1][x + 1]
+            bottom_left = matrix[y + 1][x - 1]
+            bottom_right = matrix[y + 1][x + 1]
+
+            if (
+                MAS_MAP.get(top_left) != bottom_right
+                or MAS_MAP.get(bottom_left) != top_right
+            ):
+                continue
+
+            count += 1
+
+    return count
+
+
 if __name__ == "__main__":
     input_path = "../input/day04"
     with open(input_path, "r", encoding="utf-8") as f:
         input_ = list(f)
 
     matrix = [list(line) for line in input_]
+    # matrix = [list(line) for line in INPUT.split("\n")]
 
     print("Part 1:")
     print(part_1(matrix))
+
+    print("Part 2:")
+    print(part_2(matrix))
