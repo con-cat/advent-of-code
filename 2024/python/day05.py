4260ec123b06bf3fd4f62e9d379460ae4d3d91b1
Caitlin White <caitlin@elephants.email>
2024-12-16 17:18:49 +1100

    Day 5 part 2 in Python

--- 2024/python/day05.py
+++ 2024/python/day05.py
@@ -18,6 +18,49 @@ def part_1(rules: list[tuple[str, str]], updates: list[list[str]]) -> int:
     return total
 
 
+def part_2(rules: list[tuple[str, str]], updates: list[list[str]]) -> int:
+    total = 0
+    for pages in updates:
+        indices = {page: i for i, page in enumerate(pages)}
+        for rule in rules:
+            try:
+                is_in_order = indices[rule[0]] < indices[rule[1]]
+            except KeyError:
+                continue
+
+            if not is_in_order:
+                break
+
+        if not is_in_order:
+            ordered = order_indices(indices, rules)
+            index = int(len(pages) / 2)
+            sorted_pages = [
+                page for page, _ in sorted(ordered.items(), key=lambda item: item[1])
+            ]
+            total += int(sorted_pages[index])
+
+    return total
+
+
+def order_indices(
+    indices: dict[str, int], rules: list[tuple[str, str]]
+) -> dict[str, int]:
+    for rule in rules:
+        try:
+            first_rule_index = indices[rule[0]]
+            second_rule_index = indices[rule[1]]
+            is_in_order = first_rule_index < second_rule_index
+        except KeyError:
+            continue
+
+        if not is_in_order:
+            indices[rule[0]] = second_rule_index
+            indices[rule[1]] = first_rule_index
+            return order_indices(indices, rules)
+
+    return indices
+
+
 if __name__ == "__main__":
     rules = []
     updates = []
@@ -37,3 +80,6 @@ if __name__ == "__main__":
 
     print("Part 1:")
     print(part_1(rules, updates))
+
+    print("Part 2:")
+    print(part_2(rules, updates))
