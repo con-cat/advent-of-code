56114cc64e1efb34fc76f7cd56fe417570c81e30
Caitlin White <caitlin@elephants.email>
2025-12-07 18:24:01 +0930

    Day 6 part 2 in Python

--- 2025/python/day06.py
+++ 2025/python/day06.py
@@ -14,7 +14,7 @@ OPERATIONS = {
 def part_1(input_path: str) -> int:
     grand_total = 0
 
-    problems = parse_input_file(input_path)
+    problems = parse_input_part_1(input_path)
     for problem in problems:
         operation = problem[0]
         numbers = problem[1]
@@ -24,7 +24,7 @@ def part_1(input_path: str) -> int:
     return grand_total
 
 
-def parse_input_file(input_path: str) -> list[tuple[Callable, list[int]]]:
+def parse_input_part_1(input_path: str) -> list[tuple[Callable, list[int]]]:
     split_lines = []
     with open(input_path, "r", encoding="utf-8") as f:
         for line in f.readlines():
@@ -48,5 +48,67 @@ def parse_input_file(input_path: str) -> list[tuple[Callable, list[int]]]:
     return problems
 
 
+def part_2(input_path: str) -> list[list[Callable, list[int]]]:
+    with open(input_path, "r", encoding="utf-8") as f:
+        # Reverse the characters in each line
+        lines = f.readlines()
+
+    operations = lines.pop()
+
+    problem_map = _build_problem_map(operations)
+
+    grand_total = 0
+    for (start, end), operation in problem_map.items():
+        problem_lines = [line[start:end] for line in lines]
+        numbers = _parse_problem(problem_lines)
+        subtotal = functools.reduce(lambda x, y: operation(x, y), numbers)
+        grand_total += subtotal
+
+    return grand_total
+
+
+def _build_problem_map(operations: list[str]) -> dict[tuple[int], Callable]:
+    """Build a dictionary of start and end indices of problems and their operations."""
+    problem_map: dict[tuple[int, int], Callable] = {}
+    previous_problem_start_index = 0
+    previous_operation = None
+    for i, char in enumerate(operations):
+        try:
+            operation = OPERATIONS[char]
+        except KeyError:
+            continue
+        else:
+            problem_start_index = i
+            if previous_operation is not None:
+                problem_map[(previous_problem_start_index, problem_start_index)] = (
+                    previous_operation
+                )
+            previous_operation = operation
+            previous_problem_start_index = problem_start_index
+
+    problem_map[(problem_start_index, len(operations) + 1)] = operation
+
+    return problem_map
+
+
+def _parse_problem(problem: list[str]) -> list[str]:
+    numbers = []
+    # Rotate the lines of the problem 90 degrees
+    transposed = [list(row) for row in zip(*problem)]
+
+    for row in transposed:
+        number_str = "".join(row)
+        try:
+            number = int(number_str)
+        except ValueError:
+            continue
+
+        numbers.append(number)
+
+    return numbers
+
+
 if __name__ == "__main__":
-    print(part_1(INPUT_PATH))
+    assert part_2(TEST_INPUT_PATH) == 3263827
+
+    print(part_2(INPUT_PATH))
